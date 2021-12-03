9261bb8e54b919376f38475a83d4c163f3bb1e83
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-12-03 10:55:03 +0800

    Fetching data

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import {Row, Col} from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 function HomeScreen() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        async function fetchProducts() {
+            const { data } = await axios.get('/api/products/')
+            setProducts(data)
+        }
+
+        fetchProducts()
+    }, [])
+
     return (
         <div>
             <h1>Latest Products </h1>
             <Row>
                 {products.map(product => (
-                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                    <Product product={product} />
-                                </Col>
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                    </Col>
                 ))}
             </Row>
         </div>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
-function ProductScreen ({match}) {
-    const product = products.find ((p) => p._id == match.params.id)
+function ProductScreen({ match }) {
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        async function fetchProduct() {
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+
+        fetchProduct()
+    }, [])
+
     return (
         <div>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
             <Row>
                 <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid/>
+                    <Image src={product.image} alt={product.name} fluid />
                 </Col>
                 <Col md={3}>
                     <ListGroup variant="flush">
@@ -20,7 +30,7 @@ function ProductScreen ({match}) {
                         </ListGroup.Item>
 
                         <ListGroup.Item>
-                            <Rating value={product.rating} text={'${product.numReviews}reviews'} color ={'#f8e825'}/>
+                            <Rating value={product.rating} text={`${product.numReviews}reviews`} color={'#f8e825'} />
                         </ListGroup.Item>
 
                         <ListGroup.Item>
