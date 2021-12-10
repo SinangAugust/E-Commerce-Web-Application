11c762c7a8126162c89d247e9e19e35fd4a257ce
August <august.sinang@gmail.com>
2021-12-10 15:55:34 +0800

    Serving & Fetching Data From Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,5 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'frontend/build/static'
+]
+
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 CORS_ALLOW_ALL_ORIGINS = True
