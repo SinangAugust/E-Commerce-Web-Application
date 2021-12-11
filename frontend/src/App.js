c1ccc79d0e9d787ef0604c032c8ff807e9347704
audryy <maanoaudrey@gmail.com>
2021-12-11 09:33:21 +0800

    Adding To Shopping Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <main className="py-3">
         <Container>
             <Route path='/' component={HomeScreen} exact/>
-            <Route path='/product/:id' component={ProductScreen} exact/>
+            <Route path='/product/:id' component={ProductScreen} />
+            <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer/>
---
+++ frontend/src/constants/cartConstants.js
@@ -0,0 +1,2 @@
+export const = CART_ADD_IEM = 'CART_ADD_ITEM'
+export const = CART_REMOVE_IEM = 'CART_REMOVE_ITEM'
