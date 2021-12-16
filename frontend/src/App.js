9b5d617b2dc4ebc8ba161d007c111e7b668c1e85
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-12-16 08:15:35 +0800

    Fixed bug - empty cart and order not found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -17,22 +17,22 @@ import OrderScreen from './screens/OrderScreen'
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <main className="py-3">
         <Container>
-            <Route path='/' component={HomeScreen} exact/>
-            <Route path='/login' component={LoginScreen} />
-            <Route path='/register' component={RegisterScreen} />
-            <Route path='/profile' component={ProfileScreen} />
-            <Route path='/shipping' component={ShippingScreen} />
-            <Route path='/placeorder' component={PlaceOrderScreen} />
-            <Route path='/order' component={OrderScreen} />
-            <Route path='/payment' component={PaymentScreen} />
-            <Route path='/product/:id' component={ProductScreen} />
-            <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
